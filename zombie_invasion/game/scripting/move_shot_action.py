bc20681d23c7588814fce4d60337150cbdc724e8
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-26 18:26:08 -0300

    implemented move shot action class

---
+++ zombie_invasion/game/scripting/move_shot_action.py
@@ -0,0 +1,28 @@
+from constants import *
+from game.casting.sound import Sound
+from game.casting.point import Point
+from game.scripting.action import Action
+
+
+class MoveShotAction(Action):
+
+    def __init__(self, physics_service, audio_service):
+        self._physics_service = physics_service
+        self._audio_service = audio_service
+        
+    def execute(self, cast, script, callback):
+    
+        bullets = cast.get_actors(BULLET_GROUP)
+        for bullet in bullets:
+            body = bullet.get_body()
+            velocity = body.get_velocity()
+            position = body.get_position()
+            y = position.get_y()    
+            position = position.add(velocity)
+
+            if y < 0:
+                position = Point(position.get_y(), 0)
+            elif y > (SCREEN_HEIGHT - BULLET_HEIGHT):
+                position = Point(position.get_x(), SCREEN_HEIGHT - BULLET_HEIGHT)
+                
+            body.set_position(position)
