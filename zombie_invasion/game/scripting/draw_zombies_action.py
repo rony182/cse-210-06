32e90c02010a50f7756edeb59c603bf4494c75a2
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-26 20:27:02 -0300

    fixed init method
    Co-authored-by: BelChaparro <BelChaparro@users.noreply.github.com>
    Co-authored-by: Rony Calderon <rony182@users.noreply.github.com>
    Co-authored-by: kwazeme <kwazeme@users.noreply.github.com>

--- zombie_invasion/game/scripting/draw_zombies_action.py
+++ zombie_invasion/game/scripting/draw_zombies_action.py
@@ -3,7 +3,7 @@ from game.scripting.action import Action
 
 class DrawZombiesAction(Action):
 
-    def __innit__(self, video_service):
+    def __init__(self, video_service):
         self._video_service = video_service
     
     def execute(self, cast, script, callback):
