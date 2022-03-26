5432f73cbf7a5bed65019c768ea707325f5fee18
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-26 20:23:26 -0300

    removed player initialization

--- zombie_invasion/game/scripting/collide_zombie_action.py
+++ zombie_invasion/game/scripting/collide_zombie_action.py
@@ -9,11 +9,10 @@ class CollideZombieAction(Action):
     def __init__(self, physics_service, audio_service):
         self._physics_service = physics_service
         self._audio_service = audio_service
-        self._player= Player()
         
     def execute(self, cast, script, callback):
         zombies = cast.get_actors(ZOMBIE_GROUP)
-        player = self._player
+        player = cast.get_first_actor(PLAYER_GROUP)
         
         for zombie in zombies:
             zombie_body= zombie.get_body()
