c0a85fa9011d924faaa6fac96f2a7e4688bd8d6e
BelChaparro <a.belenchaparro@gmail.com>
2022-03-26 17:02:13 -0300

    created" draw_zombies_action" file and class

---
+++ zombie_invasion/game/scripting/draw_zombies_action.py
@@ -0,0 +1,21 @@
+from constants import *
+from game.scripting.action import Action
+
+class DrawZombiesAction(Action):
+
+    def __innit__(self, video_service):
+        self._video_service = video_service
+    
+    def execute(self, cast, script, callback):
+        zombies = cast.get_actors(ZOMBIE_GROUP)
+
+        for zombie in zombies:
+            body = zombie.get_body()
+
+            # if zombie.is_debug():
+            #     background_rectangle = body.get_rectangle()
+            #     self._video_service.draw_rectangle(background_rectangle, BLACK)
+            
+            image = zombie.get_image()
+            position = body.get_position()
+            self._video_service.draw_image(image, position)
