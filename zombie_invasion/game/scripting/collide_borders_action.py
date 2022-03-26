8cbe24a5c19a8dacf2b6e65af4715dda843c1537
BelChaparro <a.belenchaparro@gmail.com>
2022-03-26 17:24:10 -0300

    created "collide_borders_action" file and class

---
+++ zombie_invasion/game/scripting/collide_borders_action.py
@@ -0,0 +1,12 @@
+from constants import *
+from game.casting.sound import Sound
+from game.scripting.action import Action
+
+class CollideBordersAction(Action):
+
+    def __init__(self, physics_service, audio_service):
+        self._physics_service = physics_service
+        self._audio_service = audio_service    
+        
+    def execute(self, cast, script, callback):
+        pass
