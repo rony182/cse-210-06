01c4c844489e977280ed0d5a4e81144492cade52
kwazeme <ogu21006@byui.edu>
2022-03-26 18:04:53 +0200

    move player

---
+++ zombie_invasion/game/scripting/move_player_action.py
@@ -0,0 +1,25 @@
+from constants import *
+from game.casting.point import Point
+from game.scripting.action import Action
+
+
+class MovePlayerAction(Action):
+
+    def __init__(self):
+        pass
+
+    def execute(self, cast, script, callback):
+        gun = cast.get_first_actor(GUNNER_GROUP)
+        body = gun.get_body()
+        velocity = body.get_velocity()
+        position = body.get_position()
+        x = position.get_x()
+        
+        position = position.add(velocity)
+
+        if x < 0:
+            position = Point(0, position.get_y())
+        elif x > (SCREEN_WIDTH - GUNNER_WIDTH):
+            position = Point(SCREEN_WIDTH - GUNNER_WIDTH, position.get_y())
+            
+        body.set_position(position)
