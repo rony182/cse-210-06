c01e8707976e0a95d43f60f0148232f0421eaac4
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-30 11:48:29 -0300

    Changed player img

--- zombie_invasion/game/directing/scene_manager.py
+++ zombie_invasion/game/directing/scene_manager.py
@@ -92,7 +92,7 @@ class SceneManager:
 
 
     # Player Image Filepath
-    PLAYER_FILEPATH = "zombie_invasion\\assets\\images\\player_1.png"
+    PLAYER_FILEPATH = "zombie_invasion\\assets\\images\\player_1_30px.png"
 
     def __init__(self):
         pass
