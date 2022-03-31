25b70334099b59528c4bc0232e2c0feed4fbf6bd
BelChaparro <a.belenchaparro@gmail.com>
2022-03-31 00:40:48 -0300

    Added the feature of -1 life when 10 zombies breach the bottom border

--- zombie_invasion/game/scripting/collide_borders_action.py
+++ zombie_invasion/game/scripting/collide_borders_action.py
@@ -7,7 +7,8 @@ class CollideBordersAction(Action):
 
     def __init__(self, physics_service, audio_service):
         self._physics_service = physics_service
-        self._audio_service = audio_service    
+        self._audio_service = audio_service
+        self._breach_count = 0
         
     def execute(self, cast, script, callback):
         
@@ -21,3 +22,29 @@ class CollideBordersAction(Action):
             
         elif x >= (FIELD_RIGHT - PLAYER_WIDTH):
             player.stop_moving()
+
+        zombies = cast.get_actors(ZOMBIE_GROUP)
+        for zombie in zombies:
+            zombie_body = zombie.get_body()
+            zombie_position = zombie_body.get_position()
+            y = zombie_position.get_y()
+
+            if y >= (FIELD_BOTTOM):
+                stats = cast.get_first_actor(STATS_GROUP)
+                self._breach_counter()
+                if self._breach_count >= 10:
+                    stats.lose_life()
+                    self._reset_breach_counter()
+            
+                if stats.get_lives() == 0:
+                    callback.on_next(GAME_OVER)
+                    sound = Sound(OVER_SOUND)
+                    self._audio_service.play_sound(sound)
+    
+    def _breach_counter(self):
+        """Counts how many zombies passed by the player."""
+        self._breach_count += 1
+    
+    def _reset_breach_counter(self):
+        """Resets the breach counter."""
+        self._breach_count = 0
